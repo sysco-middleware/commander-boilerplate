8f801e9c592127c79cd3de0f052602d844ff1fa5
Jeroen Rinzema <jeroen.rinzema@sysco.no>
2018-07-02 23:23:45 +0200

    fix: store commander in a seperate package

---
+++ service/common/main.go
@@ -0,0 +1,8 @@
+package common
+
+import "github.com/sysco-middleware/commander"
+
+var (
+	// Commander stores the global commander struct
+	Commander *commander.Commander
+)
--- service/main.go
+++ service/main.go
@@ -5,10 +5,9 @@ import (
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/sysco-middleware/commander"
+	"github.com/sysco-middleware/commander-boilerplate/service/common"
 	"github.com/sysco-middleware/commander-boilerplate/service/controllers"
 )
-
-var cmd *commander.Commander
 
 func main() {
 	servers := os.Getenv("KAFKA_SERVERS")
@@ -24,15 +23,15 @@ func main() {
 		"default.topic.config": kafka.ConfigMap{"auto.offset.reset": "earliest"},
 	})
 
-	cmd = &commander.Commander{
+	common.Commander = &commander.Commander{
 		Consumer: consumer,
 		Producer: producer,
 	}
 
-	cmd.CloseOnSIGTERM()
-	cmd.StartConsuming()
+	common.Commander.NewCommandHandle("Create", controllers.OnCreateUser)
+	common.Commander.NewCommandHandle("Update", controllers.OnUpdateUser)
+	common.Commander.NewCommandHandle("Delete", controllers.OnDeleteUser)
 
-	cmd.NewCommandHandle("create", controllers.OnCreateUser)
-	cmd.NewCommandHandle("update", controllers.OnUpdateUser)
-	cmd.NewCommandHandle("delete", controllers.OnDeleteUser)
+	common.Commander.CloseOnSIGTERM()
+	common.Commander.StartConsuming()
 }
