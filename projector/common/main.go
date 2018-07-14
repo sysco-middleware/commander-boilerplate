bf46f7025d3a438109f8cd44cf83e4d15f6b611c
Jeroen Rinzema <jeroen.rinzema@sysco.no>
2018-07-14 22:48:17 +0200

    feat: common variables

--- projector/common/main.go
+++ projector/common/main.go
@@ -1,8 +1,13 @@
 package common
 
-import "github.com/sysco-middleware/commander"
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/sysco-middleware/commander"
+)
 
 var (
 	// Commander holds the global commander struct
 	Commander *commander.Commander
+	// Database holds the global database struct
+	Database *gorm.DB
 )
