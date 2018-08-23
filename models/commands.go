05ba850434befdc252d9f7b37f13aef4ed2ad95c
Jeroen Rinzema <jeroen.rinzema@sysco.no>
2018-08-23 08:45:20 +0200

    feat: UserDeleteCommand

--- models/commands.go
+++ models/commands.go
@@ -1,7 +1,14 @@
 package models
 
+import uuid "github.com/satori/go.uuid"
+
 // UserCreatedCommand holds the content of a "created" command
 type UserCreatedCommand struct {
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
+}
+
+// UserDeleteCommand holds the content of a "delete" command
+type UserDeleteCommand struct {
+	ID *uuid.UUID `json:"id"`
 }
